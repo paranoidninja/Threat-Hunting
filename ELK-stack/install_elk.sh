f16adb38e6b0af2c8dd17eb64dcf60eecc230f12
Paranoid Ninja <paranoidninja@protonmail.com>
2017-09-14 05:46:37 +0000

    Update install_elk.sh

--- ELK-stack/install_elk.sh
+++ ELK-stack/install_elk.sh
@@ -26,6 +26,7 @@ Elastic() {
 	#Changing Network Hosts
 	echo -e "`tput setaf 3`\n[+] Modifying Network Details...\n`tput setaf 7`"
 	sed -i "s/\#network.host: 192.168.0.1/network.host: 127.0.0.1/g" /etc/elasticsearch/elasticsearch.yml
+	echo "http.host: 0.0.0.0" >> /etc/elasticsearch/elasticsearch.yml
 
 	#Disabling Java Swap for Elasticsearch only
 	if [ "$disable_swap" = true ]; then
